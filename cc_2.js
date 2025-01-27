831105d9b8616584bcfaa8546e8b8f1d22710d98
bojokoeva <bojokoeva34@gmail.com>
2025-01-27 00:06:04 -0500

    javascript2

--- cc_2.js
+++ cc_2.js
@@ -8,3 +8,15 @@ products.pop();
 
 // Log the updated list
 console.log(products);
+
+let scores = [85, 90, 78, 92, 88];
+
+// Update the second score
+scores[1] = 95;
+
+// Calculate the average score
+let average = scores.reduce((total, score) => total + score, 0) / scores.length;
+
+// Log the updated array and average score
+console.log(scores);
+console.log("Average score:", average);
