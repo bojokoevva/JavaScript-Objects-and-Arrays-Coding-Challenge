7e4e84cefb55376727a987f47b90efbc245db71f
bojokoeva <bojokoeva34@gmail.com>
2025-01-27 00:05:08 -0500

    javascript2

---
+++ cc_2.js
@@ -0,0 +1,10 @@
+let products = ["Laptop", "Smartphone", "Headphones", "Monitor", "Keyboard"];
+
+// Add a new product
+products.push("Mouse");
+
+// Remove the last product
+products.pop();
+
+// Log the updated list
+console.log(products);
